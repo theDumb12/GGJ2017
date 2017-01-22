ff2b18b7e4e0c3a3b31463e1f17ad2baa2730fde
Evan Cooper <Evan Cooper>
2017-01-22 15:55:16 -0800

    Added game exe, changes to enemy anims, and other

---
+++ GGJ2017/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject textObject;
+    public string level;
+    public Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        Text title = textObject.GetComponent<Text>();
+        title.font.material.mainTexture.filterMode = FilterMode.Point;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        button.onClick.AddListener(LoadLevel);
+    }
+
+    void LoadLevel()
+    {
+        //Application.LoadLevel(level);
+        SceneManager.LoadScene(level);
+    }
+}
